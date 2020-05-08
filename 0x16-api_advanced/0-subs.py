b14d1165293a1e9e88f6660e151d88cc7b57321b
David <stevenbravobel@gmail.com>
2020-05-08 02:35:39 +0000

    [Fix] file 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """returns the number of subscribers from Reddit"""
     headers = {'User-agent': 'Mozilla/5.0'}
     req = requests.get("https://www.reddit.com/r/{}/about.json"
                          .format(subreddit), headers=headers)
