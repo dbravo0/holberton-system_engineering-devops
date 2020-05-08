a073c3a5a7d4fcd7115c6f9617a45c0335b68094
David <stevenbravobel@gmail.com>
2020-05-08 02:46:52 +0000

    [Fix] file 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,9 +5,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers from Reddit"""
-    headers = {'User-agent': 'Mozilla/5.0'}
     req = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subreddit), headers=headers)
+                       .format(subreddit), headers={'user-agent': 'head'})
     if req.status_code == 200:
         return req.json()['data']['subscribers']
     return 0
