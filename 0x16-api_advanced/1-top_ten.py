c1a31985e607e2532db4fb4203dcc02b07f6a8a7
David <stevenbravobel@gmail.com>
2020-05-08 04:20:53 +0000

    [Fix] file 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,9 +7,9 @@ def top_ten(subreddit):
     headers = {'user-agent': 'head'}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     req = requests.get(url,
-                     params={'limit': 10},
-                     headers=headers,
-                     allow_redirects=False)
+                       params={'limit': 10},
+                       headers=headers,
+                       allow_redirects=False)
 
     if req.status_code != 200:
         print(None)
