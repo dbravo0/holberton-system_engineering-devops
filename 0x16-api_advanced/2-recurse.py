88a3edf371f1ec41e31793433da52ef232849d91
David <stevenbravobel@gmail.com>
2020-05-08 04:00:34 +0000

    [Fix] file 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,21 +4,21 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
+    headers = {'user-agent': 'head'}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    headers = {'user-agent': 'head'},
     req = requests.get(url,
                        params={'after': after},
                        headers=headers,
                        allow_redirects=False)
 
     if req.status_code != 200:
-        return None
+        return (None)
     else:
         req_json = req.json()
         after = req_json['data'].get('after')
         for posts in req_json['data']['children']:
             hot_list.append(posts['data'].get('title'))
         if after is None:
-            return hot_list
+            return (hot_list)
         else:
-            return recurse(subreddit, hot_list, after)
+            return (recurse(subreddit, hot_list, after))
